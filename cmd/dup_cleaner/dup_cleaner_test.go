706a03206ec8949d7d2a898595b4fd0f58c374e4
Dmitriy Rekunov <laddr@mail.ru>
2021-11-20 14:07:06 +0300

    Lesson-2 logger added

---
+++ cmd/dup_cleaner/dup_cleaner_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestGetFileList(t *testing.T) {
+	a := app{}
+	if a.InitApp() != nil {
+		panic("Error init dup_cleaner")
+	}
+	defer a.CloseApp()
+
+	list, err := a.getFileList("./")
+	if err != nil {
+		t.Error("err!=nil")
+	}
+	if len(list) != 3 {
+		t.Error("len(list)!=3")
+	}
+	if list[0].fs.Name() != "dup_cleaner.go" {
+		t.Error("list[0].fs.Name()!=dup_cleaner.go")
+	}
+	if list[1].fs.Name() != "dup_cleaner_test.go" {
+		t.Error("list[1].fs.Name()!=dup_cleaner_test.go")
+	}
+	if list[2].fs.Name() != "test1.txt" {
+		t.Error("list[2].fs.Name()!=test.txt")
+	}
+}
